dca9b17080af5555bd6beb5e530f15861b38fcd1
Anastasiia Kudryakova <nasty.kudryakova@yandex.ru>
2025-04-01 20:12:01 +0200

    hw10

--- homework-solutions/js/hw-10/task-2.mjs
+++ homework-solutions/js/hw-10/task-2.mjs
@@ -6,9 +6,19 @@
   
   Ожидается: { 1: 1, 2: 2, 3: 1, 4: 3, 5: 1 }
 */
+const numbers = [1, 2, 2, 3, 4, 4, 4, 5]
 
 function countOccurrences(arr) {
-  // ваш код
+const counterObject = {}
+for (const num of arr) {
+  counterObject[num] = (counterObject[num] || 0) + 1
 }
+return counterObject
+
+
+}
+console.log(countOccurrences(numbers))
+
+
 
 export { countOccurrences };
